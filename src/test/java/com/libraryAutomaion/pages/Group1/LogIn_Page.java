9585b5aaf1fb937f679576e41b5a5b626895d650
alimjanyunus <alimjan.yunus@gmail.com>
2021-02-20 16:30:41 -0500

    Alimjan Finished Assignment

--- src/test/java/com/libraryAutomaion/pages/Group1/LogIn_Page.java
+++ src/test/java/com/libraryAutomaion/pages/Group1/LogIn_Page.java
@@ -1,5 +1,24 @@
 package com.libraryAutomaion.pages.Group1;
 
+import com.libraryAutomaion.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import javax.xml.xpath.XPath;
+
 public class LogIn_Page {
+
+    public LogIn_Page(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(id = "inputEmail")
+    public WebElement inputEmail;
+
+    @FindBy(id = "inputPassword")
+    public WebElement inputPassword;
+
+    @FindBy(xpath = "//button[.='Sign in']")
+    public WebElement buttonSignIn;
 
 }
--- src/test/java/com/libraryAutomaion/pages/Group2/LogIn_Page.kt
+++ src/test/java/com/libraryAutomaion/pages/Group2/LogIn_Page.kt
@@ -1,4 +1,6 @@
 package com.libraryAutomaion.pages.Group2
 
 class LogIn_Page {
+
+
 }
